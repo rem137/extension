8694e81ee81ebaa020f2906520d9454f17f60514
rem137 <56608633+rem137@users.noreply.github.com>
2020-05-24 15:00:44 +0200

    Update randomtest.js

--- randomtest.js
+++ randomtest.js
@@ -1,8 +1,16 @@
-MessageBot.registerExtension('rem137/test', function(ex, world) {
-   ex.world.addCommand('ping', function(player, args) {
-ex.bot.send('pong')
-});
-ex.remove = function(){
-world.removeCommand('ping')
-}
-});
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
+  typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
+  (factory(global['@bhmb/bot']));
+}(this, (function (bot) { 'use strict';
+  const MessageBot = bot.MessageBot
+  MessageBot.registerExtension('test', function(ex, world) {
+    world.addCommand('ping', function(player, args) {
+      ex.bot.send('Pong!')
+    })
+
+    ex.remove = function() {
+      world.removeCommand('ping')
+    }
+  })
+})))
