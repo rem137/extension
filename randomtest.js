22e9f158463079a8ba75f5773df4cbc51969541a
rem137 <56608633+rem137@users.noreply.github.com>
2020-05-24 20:26:24 +0200

    Update randomtest.js

--- randomtest.js
+++ randomtest.js
@@ -1,16 +1,12 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
-  typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
-  (factory(global['@bhmb/bot']));
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(require('@bhmb/bot')) :
+    typeof define === 'function' && define.amd ? define(['@bhmb/bot'], factory) :
+    (factory(global['@bhmb/bot']));
 }(this, (function (bot) { 'use strict';
-  const MessageBot = bot.MessageBot
-  MessageBot.registerExtension('bibliofile/marcopolo', function(ex, world) {
-    world.addCommand('marco', function(player, args) {
-      ex.bot.send('Poloo!')
+    const MessageBot = bot.MessageBot
+    MessageBot.registerExtension('bibliofile/marcopolo', function(ex, world) {
+        world.addCommand('marco', function(player, args) {
+            ex.bot.send('Polo!')
+        })
     })
-
-    ex.remove = function() {
-      world.removeCommand('marco')
-    }
-  })
 })))
